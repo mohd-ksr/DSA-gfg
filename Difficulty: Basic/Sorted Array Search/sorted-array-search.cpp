d1a51672766b8e5dd098305babdcdb53ea17a097
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2025-11-29 21:38:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sorted Array Search/sorted-array-search.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    bool searchInSorted(vector<int>& arr, int k) {
+        // code here
+        int s=0, e = arr.size()-1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(arr[mid]==k)return true;
+            else if(arr[mid]>k)e=mid-1;
+            else s=mid+1;
+        }
+        return false;
+    }
+};
