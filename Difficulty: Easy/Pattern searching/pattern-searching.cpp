6deaa42727327f3bf0c1505479f2f3efb8672c0b
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-01-29 15:35:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pattern searching/pattern-searching.cpp
@@ -0,0 +1,9 @@
+// User function Template for C++
+class Solution {
+  public:
+    bool searchPattern(string& txt, string& pat) {
+        // your code here
+         return txt.find(pat) != string::npos;
+
+    }
+};
