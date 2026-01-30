7c29cc014f6765e5dfd4489002e06f6bca1f3e6e
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-01-30 11:34:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,35 +1,30 @@
 class Solution {
 private:
-    bool bfs(int src,  vector<vector<int>>&adjList, vector<bool>&vis){
-        vis[src]=true;
-        queue<pair<int, int>>q;
-        q.push({src,-1});
+    bool dfs(int node, int parent, vector<vector<int>>&adjList, vector<bool>&vis){
+        vis[node]=true;
         
-        while(!q.empty()){
-            auto [node, parent] = q.front();q.pop();
-            for(auto &nei:adjList[node]){
-                if(!vis[nei]){
-                    vis[nei]=true;
-                    q.push({nei, node});
-                }
-                else if(nei!=parent)return true;
+        for(int nei:adjList[node]){
+            if(!vis[nei]){
+                if(dfs(nei, node, adjList, vis))return true;
             }
+            else if(parent!=nei)return true;
         }
         return false;
     }
 public:
     bool isCycle(int V, vector<vector<int>>& edges) {
-        // Code here
         vector<vector<int>>adjList(V);
         for(auto &e:edges){
             adjList[e[0]].push_back(e[1]);
             adjList[e[1]].push_back(e[0]);
         }
         
-        vector<bool>vis(V,false);
+        vector<bool>vis(V, false);
         for(int i=0; i<V; i++){
-            if(!vis[i] && bfs(i, adjList, vis))return true;
+            if(!vis[i] && dfs(i,-1,adjList,vis)) return true;
         }
+        
         return false;
+        
     }
 };
