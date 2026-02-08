611540ad0a76890c2d7e73c52f3588c7adfd4413
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-08 21:12:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -17,17 +17,38 @@ private:
 public:
     bool isCyclic(int V, vector<vector<int>> &edges) {
         vector<vector<int>>adjList(V);
+        vector<int>inDeg(V, 0);
         for(auto &e:edges){
             adjList[e[0]].push_back(e[1]);
+            inDeg[e[1]]++;
         }
         
-        vector<int>vis(V, 0), pathVis(V, 0);
+        // vector<int>vis(V, 0), pathVis(V, 0);
         
+        // for(int i=0; i<V; i++){
+        //     if(!vis[i]){
+        //         if(dfs(i, adjList, vis, pathVis)) return true;
+        //     }
+        // }
+        // return false;
+        int count=0;
+        queue<int>q;
         for(int i=0; i<V; i++){
-            if(!vis[i]){
-                if(dfs(i, adjList, vis, pathVis)) return true;
-            }
+            if(inDeg[i]==0)q.push(i);
         }
-        return false;
+        while(!q.empty()){
+            int node = q.front(); q.pop();
+            
+            for(int nei:adjList[node]){
+                inDeg[nei]--;
+                if(inDeg[nei]==0)q.push(nei);
+            }
+            count++;
+        }
+        return !(count==V);
     }
 };
+
+
+
+
