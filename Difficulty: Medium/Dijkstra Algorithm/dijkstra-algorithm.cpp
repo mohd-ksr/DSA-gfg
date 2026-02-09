845c4a35f1f6ee13dfe1949c66b80d254261ac61
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-09 15:20:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -1,26 +1,25 @@
 class Solution {
+private:
+    const int inf = 1e9+7;
   public:
-  const int inf = 1e9+7;
     vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
-        vector<vector<pair<int, int>>>adjList(V);
+        // Code here
+        vector<vector<pair<int, int>>>adj(V);
         for(auto &e:edges){
-            adjList[e[0]].push_back({e[2], e[1]});
-            adjList[e[1]].push_back({e[2], e[0]});
+            adj[e[0]].push_back({e[1], e[2]});
+            adj[e[1]].push_back({e[0], e[2]});
         }
-        
         vector<int>dist(V, inf);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>>pq;
+        priority_queue<pair<int, int>,vector<pair<int, int>>, greater<pair<int, int>>>pq;
         pq.push({0, src});
         dist[src]=0;
         while(!pq.empty()){
-            auto [d, u] = pq.top(); pq.pop();
-            
-            if(d>dist[u])continue;
-            
-            for(auto [w, v]:adjList[u]){
-                if(dist[u]+w < dist[v]){
-                    dist[v]=dist[u]+w;
-                    pq.push({dist[v], v});
+            auto [d, node]=pq.top(); pq.pop();
+            if(d>dist[node])continue;
+            for(auto [nei, w]:adj[node]){
+                if(dist[node]+w < dist[nei]){
+                    dist[nei]=dist[node] + w;
+                    pq.push({dist[nei], nei});
                 }
             }
         }
