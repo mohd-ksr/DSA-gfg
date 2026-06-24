25460ff3652e4582503665a6d60f7444e3160910
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-06-24 13:14:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number Of Islands/number-of-islands.cpp
+++ Difficulty: Medium/Number Of Islands/number-of-islands.cpp
@@ -1,26 +1,29 @@
 class DSU{
 private:
-    vector<int>parent, rank;
+    vector<int>rank,parent;
 public:
     DSU(int n){
-        parent.resize(n+1);
         rank.resize(n+1, 0);
-        for(int i=0; i<=n; i++)parent[i]=i;
+        parent.resize(n+1);
+        for(int i=0; i<=n; i++){
+            parent[i]=i;
+        }
     }
     
-    int findUpar(int node){
-        if(parent[node]==node)return node;
-        return parent[node]=findUpar(parent[node]);
+    int findUlpParent(int node){
+        if(node==parent[node])return node;
+        return parent[node]=findUlpParent(parent[node]);
     }
     
     void unionByRank(int u, int v){
-        int ulp_u = findUpar(u);
-        int ulp_v = findUpar(v);
+        int ulp_u = findUlpParent(u);
+        int ulp_v = findUlpParent(v);
+        if(ulp_u==ulp_v)return;
         
         if(rank[ulp_u] < rank[ulp_v]){
             parent[ulp_u] = ulp_v;
         }
-        else if(rank[ulp_u] > rank[ulp_v]){
+        else if(rank[ulp_v] < rank[ulp_u]){
             parent[ulp_v] = ulp_u;
         }
         else{
@@ -29,46 +32,53 @@ public:
         }
     }
 };
+
 class Solution {
-public:
+  public:
     vector<int> numOfIslands(int n, int m, vector<vector<int>> &operators) {
         DSU ds(m*n);
-        vector<vector<int>>vis(n, vector<int>(m, 0));
-        int count = 0;
         vector<int>ans;
-        for(auto &op:operators){
+        vector<vector<int>>vis(n, vector<int>(m, 0));
+        int cnt=0;
+        for(auto op:operators){
             int r = op[0];
             int c = op[1];
-            
             if(vis[r][c]==1){
-                ans.push_back(count);
+                ans.push_back(cnt);
                 continue;
             }
+            
             vis[r][c]=1;
-            count++;
+            cnt++;
+            
             int dr[] = {-1, 0, 1, 0};
             int dc[] = {0, 1, 0, -1};
-            
             for(int i=0; i<4; i++){
-                int nr = r + dr[i];
-                int nc = c + dc[i];
+                int adjr = r + dr[i];
+                int adjc = c + dc[i];
                 
-                if(nr>=0 && nr<n && nc>=0 && nc<m){
-                    if(vis[nr][nc]==1){
-                        int nn = r*m + c;
-                        int nnn = nr*m + nc;
-                        if(ds.findUpar(nn) != ds.findUpar(nnn)){
-                            count--;
-                            ds.unionByRank(nn, nnn);
+                if(adjr>=0 && adjr<n && adjc>=0 && adjc<m){
+                    if(vis[adjr][adjc]==1){
+                        int nodeN = r*m+c;
+                        int adjNodeN = adjr*m+adjc;
+                        
+                        if(ds.findUlpParent(nodeN)!= ds.findUlpParent(adjNodeN)){
+                            ds.unionByRank(nodeN, adjNodeN);
+                            cnt--;
                         }
                     }
                 }
             }
-            ans.push_back(count);
+            ans.push_back(cnt);
         }
         return ans;
+        
     }
 };
+
+
+
+
 
 
 
