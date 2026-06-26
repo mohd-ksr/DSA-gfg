50c157cee92e7285166a6eb3f48157b53b27da16
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-06-26 09:14:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Strongly Connected/count-strongly-connected.cpp
@@ -0,0 +1,61 @@
+class Solution {
+private:
+    void dfs(int node, vector<int>adj[], vector<int>&vis, stack<int>&st){
+        vis[node]=1;
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                dfs(it, adj, vis, st);
+            }
+        }
+        st.push(node);
+    }
+    void dfs2(int node, vector<int>&vis, vector<int>adjT[]){
+        vis[node]=1;
+        for(auto it:adjT[node]){
+            if(!vis[it]){
+                dfs2(it, vis, adjT);
+            }
+        }
+    }
+public:
+    int kosaraju(int V, vector<vector<int>> &edges) {
+        vector<int> adj[V];
+        for(auto edge:edges){
+            adj[edge[0]].push_back(edge[1]);
+        }
+        
+        vector<int>vis(V,0);
+        stack<int>st;
+        for(int i=0; i<V; i++){
+            if(!vis[i]){
+                dfs(i, adj, vis, st);
+            }
+        }
+        
+        vector<int> adjT[V];
+        for(int i=0; i<V; i++){
+            vis[i]=0;
+            for(auto it:adj[i]){
+                adjT[it].push_back(i);
+            }
+        }
+        
+        int scc = 0;
+        while(!st.empty()){
+            int node = st.top();st.pop();
+            if(!vis[node]){
+                scc++;
+                dfs2(node, vis, adjT);
+            }
+        }
+        return scc++;
+    }
+};
+
+
+
+
+
+
+
+
