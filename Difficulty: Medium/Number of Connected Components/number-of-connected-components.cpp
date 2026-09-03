1e72d4ba0ff53fef05abc2c91d646a15deff0f60
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-09-03 23:26:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Connected Components/number-of-connected-components.cpp
+++ Difficulty: Medium/Number of Connected Components/number-of-connected-components.cpp
@@ -1,60 +1,28 @@
-class DSU{
-private:
-    vector<int>rank, parent;
-public:
-    DSU(int n){
-        rank.resize(n+1,0);
-        parent.resize(n+1);
-        for(int i=0; i<=n; i++){
-            parent[i]=i;
-        }
-    }
-    int findUlpParent(int node){
-        if(node == parent[node])return node;
-        return parent[node] = findUlpParent(parent[node]);
-    }
-    
-    void unionByRank(int u, int v){
-        int ulp_u = findUlpParent(u);
-        int ulp_v = findUlpParent(v);
-        
-        if(ulp_u == ulp_v)return;
-        
-        if(rank[ulp_u] < rank[ulp_v]){
-            parent[ulp_u] = ulp_v;
-        }
-        else if(rank[ulp_v] < rank[ulp_u]){
-            parent[ulp_v] = ulp_u;
-        }
-        else{
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-    
-    
-};
 class Solution {
+private:
+    vector<bool>vis;
+    void dfs(int node, vector<int>adj[]){
+        if(vis[node])return;
+        vis[node]=true;
+        for(int nei:adj[node]){
+            dfs(nei, adj);
+        }
+    }
 public:
     int countConnected(int V, vector<vector<int>>& edges) {
-        DSU ds(V);
+        vector<int>adj[V];
         for(auto edge:edges){
-            ds.unionByRank(edge[0], edge[1]);
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
         }
-        int ans = -1;
-        for(int i=0; i<=V; i++){
-            if(ds.findUlpParent(i)==i)ans++;
+        vis.resize(V, false);
+        int ans=0;
+        for(int i=0; i<V; i++){
+            if(!vis[i]){
+                dfs(i, adj);
+                ans++;
+            }
         }
         return ans;
-        
     }
 };
-
-
-
-
-
-
-
-
-
